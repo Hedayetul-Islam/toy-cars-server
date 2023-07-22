7c0aeb4b30381cf1c45a7b2cf22beb2f7207eda1
Hedayetul-Islam <hedayetul36@gmail.com>
2023-07-22 12:29:26 +0600

    created post api

--- index.js
+++ index.js
@@ -26,6 +26,24 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    const toyCollection = client.db('carToy').collection('cars');
+
+
+    app.post('/cars', async(req, res) => {
+      const car = req.body;
+      console.log(car);
+      const result = await toyCollection.insertOne(car);
+      res.send(result);
+    })
+
+    app.get('/cars', async(req, res) => {
+      const result = await toyCollection.find().toArray();
+      res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
