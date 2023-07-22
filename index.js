eb77dee047dc008b35df0e563447a0c5669851c1
Hedayetul-Islam <hedayetul36@gmail.com>
2023-07-22 18:44:31 +0600

    query for email sorting

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -30,6 +30,27 @@ async function run() {
 
     const toyCollection = client.db('carToy').collection('cars');
 
+    app.get('/cars/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+
+      const options = {
+          projection: { sellerName: 1, toyName: 1, subCategory: 1, price: 1, quantity: 1 },
+      };
+
+      const result = await toyCollection.findOne(query, options);
+      res.send(result);
+  })
+
+  app.get('/cars', async (req, res) => {
+    console.log(req.query.email);
+    let query = {};
+    if (req.query?.email) {
+        query = { email: req.query.email }
+    }
+    const result = await toyCollection.find(query).toArray();
+    res.send(result);
+})
 
     app.post('/cars', async(req, res) => {
       const car = req.body;
