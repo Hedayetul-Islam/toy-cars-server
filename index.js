0dc24ce84248518a160416e2dd8bce9a835afd36
Hedayetul-Islam <hedayetul36@gmail.com>
2023-07-23 18:47:23 +0600

    update api ready

--- index.js
+++ index.js
@@ -64,6 +64,24 @@ async function run() {
       res.send(result);
     })
 
+
+    app.put('/cars/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const options = { upsert: true };
+      const updateToy = req.body;
+
+      const toy = {
+          $set: {
+              price: updateToy.price, 
+              quantity: updateToy.quantity,  
+              description: updateToy.description, 
+          }
+      }
+      const result = await toyCollection.updateOne(filter, toy, options);
+      res.send(result);
+  })
+
     app.delete('/cars/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
